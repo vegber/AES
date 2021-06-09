27f30487f6528667b726e440d71356340a00dbc8
vbe020 <vbe020@uib.no>
2021-06-09 14:57:06 +0200

    Encrypt seeems to be on the right course

--- Cipher/AES.py
+++ Cipher/AES.py
@@ -1,7 +1,13 @@
+from BitVector import *
+
+from static.S_BOX import M_aes
+
+
 class Cipher:
     keys = []
     state = []
     rounds = 0
+
     def __init__(self, key, plaintext, key_size):
         self.key = key  # Key is n bits
         self.plaintext = plaintext
@@ -13,26 +19,29 @@ class Cipher:
         # turn plaintext to "matrix"
         self.state = self.tap_into_matrix(self.plaintext)
         self.add_round_key(self.state, self.keys)
-
-        for x in range(self.rounds): # n -1 rounds
-            self.subbytes(self.state)
-            self.shift_rows(self.state)
-            self.mix_columns(self.state)
-            self.add_round_key(self.state, self.keys)
+        for x in range(self.rounds):  # n -1 rounds
+            self.round_encyption()
             # Now last round
+        self.last_round()
+        return self.state
+
+    def last_round(self):
         self.subbytes(self.state)
         self.shift_rows(self.state)
         self.add_round_key(self.state, self.keys)
 
-        return self.state
+    def round_encyption(self):
+        self.subbytes(self.state)
+        self.shift_rows(self.state)
+        self.mix_columns(self.state)
+        self.add_round_key(self.state, self.keys)
 
     def round_keys(self, key, key_size):
-        #TODO
+        # TODO
         # key to matrix form
         # generate all the round keys
         # update the keys[] list
-        key_matrix = self.tap_into_matrix(key)
-        print(key_matrix)
+        self.keys.append(self.tap_into_matrix(key))
         keylist = []
         if key_size == 128:
             return keylist, 9
@@ -42,21 +51,32 @@ class Cipher:
             return keylist, 13
 
     def subbytes(self, state):
-        #TODO
+        # TODO
         pass
 
     def shift_rows(self, state):
-        #TODO
+        # TODO
         pass
 
     def mix_columns(self, state):
-        #TODO
-        pass
+        new_state = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        n = 8
+        mod = BitVector(bitstring='100011011')  # x^8 + x^4 + x^3 + x + 1
+        for y in range(4):
+            for x in range(4):
+                Yn   = BitVector(hexstring=state[y][0]).gf_multiply_modular(BitVector(intVal=M_aes[x][0]), mod, n)
+                Yn_1 = BitVector(hexstring=state[y][1]).gf_multiply_modular(BitVector(intVal=M_aes[x][1]), mod, n)
+                Yn_2 = BitVector(hexstring=state[y][2]).gf_multiply_modular(BitVector(intVal=M_aes[x][2]), mod, n)
+                Yn_3 = BitVector(hexstring=state[y][3]).gf_multiply_modular(BitVector(intVal=M_aes[x][3]), mod, n)
+                xored = Yn ^ Yn_1 ^ Yn_2 ^ Yn_3
+                new_state[x][y] = (xored.get_bitvector_in_hex())
+        return new_state
 
     def add_round_key(self, state, key_round):
         for i in range(4):
             for j in range(4):
-                state[i][j] ^= key_round[i][j]
+                pass
+                # state[i][j] ^= key_round[i][j]
 
     def tap_into_matrix(self, content):
         shaped_array, state = self.sort_array_to_matrix_state(content)
@@ -64,7 +84,7 @@ class Cipher:
             for i in range(len(state)):
                 if i % 4 == x:
                     shaped_array.append(state[i])
-        matrix = [[x for x in shaped_array[i:i+4]] for i in range(0, len(shaped_array), 4)]
+        matrix = [[x for x in shaped_array[i:i + 4]] for i in range(0, len(shaped_array), 4)]
         return matrix
 
     def sort_array_to_matrix_state(self, content):
@@ -76,10 +96,9 @@ class Cipher:
 
 
 if __name__ == '__main__':
-
     # dont need to convert this, since already hex
-    key = "2b7e151628aed2a6abf7158809cf4f3c" # .encode("utf-8").hex()
-    plaintext = "3243f6a8885a308d313198a2e0370734" # .encode("utf-8").hex()
+    key = "2b7e151628aed2a6abf7158809cf4f3c"  # .encode("utf-8").hex()
+    plaintext = "3243f6a8885a308d313198a2e0370734"  # .encode("utf-8").hex()
     cipher = Cipher(key, plaintext, 128)
 
     cipher.Encrypt()
